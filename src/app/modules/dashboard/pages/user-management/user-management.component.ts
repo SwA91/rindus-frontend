1ed9963785ec6370df051cc2d52a6ef01ed612a5
Willy Arrojas <willyarrojas@gmail.com>
2024-02-28 13:39:49 +0100

    Use classes tailwindcss

--- src/app/modules/dashboard/pages/user-management/user-management.component.ts
+++ src/app/modules/dashboard/pages/user-management/user-management.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { UserManagementFormComponent } from '@app/modules/dashboard/components/user-management/user-management-form/user-management-form.component';
+import { UserManagementTableComponent } from '@app/modules/dashboard/components/user-management/user-management-table/user-management-table.component';
 
 @Component({
   selector: 'app-user-management',
   standalone: true,
-  imports: [],
+  imports: [UserManagementTableComponent, UserManagementFormComponent],
   templateUrl: './user-management.component.html',
-  styleUrl: './user-management.component.scss'
+  styleUrl: './user-management.component.scss',
 })
-export class UserManagementComponent {
-
-}
+export class UserManagementComponent {}
